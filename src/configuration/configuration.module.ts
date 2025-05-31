447d926b754486cdb8d71ce594fd3402d7dcd496
odilon <odilongeronimoo@gmail.com>
2025-05-31 05:59:30 +0000

    feat: add Joi validation schema for database configuration and update environment files

--- src/configuration/configuration.module.ts
+++ src/configuration/configuration.module.ts
@@ -1,6 +1,42 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import Joi from 'joi';
 import { join } from 'path';
+
+type DATABASE_SCHEMA_TYPE = {
+  DB_VENDOR: 'mysql' | 'sqlite' | 'postgres';
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_DATABASE: string;
+  DB_LOGGING: boolean;
+  DB_AUTO_LOAD_MODELS: boolean;
+};
+
+export const CONFIG_DATABASE_SCHEMA: Joi.StrictSchemaMap<DATABASE_SCHEMA_TYPE> =
+  {
+    DB_VENDOR: Joi.string().required().valid('mysql', 'sqlite'),
+    DB_HOST: Joi.string().required(),
+    DB_DATABASE: Joi.string().when('DB_VENDOR', {
+      is: 'mysql',
+      then: Joi.required(),
+    }),
+    DB_USERNAME: Joi.string().when('DB_VENDOR', {
+      is: 'mysql',
+      then: Joi.required(),
+    }),
+    DB_PASSWORD: Joi.string().when('DB_VENDOR', {
+      is: 'mysql',
+      then: Joi.required(),
+    }),
+    DB_PORT: Joi.number().integer().when('DB_VENDOR', {
+      is: 'mysql',
+      then: Joi.required(),
+    }),
+    DB_LOGGING: Joi.boolean().required(),
+    DB_AUTO_LOAD_MODELS: Joi.boolean().required(),
+  };
 
 @Module({})
 export class ConfigurationModule extends ConfigModule {
@@ -15,8 +51,11 @@ export class ConfigurationModule extends ConfigModule {
           'environments',
           `.environment.${process.env.NODE_ENV}`,
         ),
-        join(process.cwd(), '.environments', `.environment`),
+        join(process.cwd(), 'environments', `.environment`),
       ],
+      validationSchema: Joi.object<DATABASE_SCHEMA_TYPE>({
+        ...CONFIG_DATABASE_SCHEMA,
+      }),
       ...otherOptions,
     });
   }
