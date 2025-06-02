9c99d7f664de016412389cff713852fbd45500bc
odilon <odilongeronimoo@gmail.com>
2025-06-02 03:41:40 +0000

    refactor: update jest type definitions for notificationContainsErrorMessages matcher

--- src/core/jest.d.ts
+++ src/core/jest.d.ts
@@ -1,11 +1,11 @@
-import type { FieldErrors } from './shared/domain/validators/interface/validator.fields.interface';
+import { ValueObject } from '../shared/domain/value-object';
+import { FieldsErrors } from './shared/domain/validators/validator-fields-interface';
 
 declare global {
   namespace jest {
     interface Matchers<R> {
-      notificationContainsErrorMessages: (
-        expected: Array<string | { [key: string]: string[] }>,
-      ) => R;
+      //containsErrorMessages: (expected: FieldsErrors) => R;
+      notificationContainsErrorMessages: (expected: Array<FieldsErrors>) => R;
       toBeValueObject: (expected: ValueObject) => R;
     }
   }
